f8d7f78c7e71b7c01011715d386f8e1e352a85be
Vicente Reyes-Puerta <vr.github@outlook.com>
2022-02-20 17:57:40 +0100

    Add python puzzle1A

---
+++ python/puzzle01.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+# Part #1
+df1 = pd.read_csv("../input/input01.txt", header=None)
+print(df1)
+df1_diff = df1.diff()
+print(df1_diff)
+df1_diff_pos = df1_diff[df1_diff > 0].dropna()
+print(df1_diff_pos)
+print("Result part #1: " + str(df1_diff_pos.shape[0]))
+
+# Part #2
