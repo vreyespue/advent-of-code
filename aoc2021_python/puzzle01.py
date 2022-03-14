60379b690e31d02a7f267a1674535098cdc4d7ce
Vicente Reyes-Puerta <vr.github@outlook.com>
2022-03-14 20:45:37 +0100

    Fix python files

--- aoc2021_python/puzzle01.py
+++ aoc2021_python/puzzle01.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Part #1
-df1 = pd.read_csv("../input/input01.txt", header=None)
+df1 = pd.read_csv("../aoc2021_input/input01.txt", header=None)
 print(df1)
 df1_diff = df1.diff()
 print(df1_diff)
